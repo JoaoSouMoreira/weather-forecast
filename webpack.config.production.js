f3c81d86f0ad43b0eea46de0e104b70783aa07c4
Joao Moreira <joaosomoreira@gmail.com>
2016-06-22 13:06:58 +0100

    Fixed syntax issues

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -55,12 +55,12 @@ module.exports = {
       ]
   },
   resolve: {
-        root: [ resolveNodePath(''), resolveModulePath('')],
-        modulesDirectories: [resolveNodePath(''), resolveModulePath('')],
-        extensions: [
-            '',
-            '.js',
-            '.html',
-            '.css', '.styl', '.less'
-        ]
-};
+    root: [ resolveNodePath(''), resolveModulePath('')],
+    modulesDirectories: [resolveNodePath(''), resolveModulePath('')],
+    extensions: [
+        '',
+        '.js',
+        '.html',
+        '.css', '.styl', '.less'
+    ]
+}
