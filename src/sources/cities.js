cb61a1c25f66e81ca55d33a6544ad294b2156c74
Joao Moreira <jmoreira@localhost.localdomain>
2016-06-22 12:03:04 +0100

    Added configuration file to hold weather key

--- src/sources/cities.js
+++ src/sources/cities.js
@@ -1,8 +1,12 @@
 import 'whatwg-fetch';
 
+import Config from '../../config.json';
+
 class CitiesSource {
   static get(cityId) {
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?id=${cityId}&appid=SECRET`, {
+    const appId = Config.WEATHER_APP_ID;
+
+    return fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?id=${cityId}&appid=${appId}`, {
       method: 'GET'
     });
   }
